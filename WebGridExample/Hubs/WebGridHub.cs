7f6201a396adb6086c11c476677636beb6fe818f
jdanylko <jdanylko@gmail.com>
2015-04-07 23:06:28 -0400

    Forgot the SignalR files

---
+++ WebGridExample/Hubs/WebGridHub.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using WebGridExample.Repository;
+
+namespace WebGridExample.Hubs
+{
+    [HubName("webGridHub")]
+    public class WebGridHub : Hub
+    {
+        private readonly UserRepository _repository;
+
+        public WebGridHub() : this(new UserRepository()) { }
+        public WebGridHub(UserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task GetPagedUsers(int page, int pageSize)
+        {
+            var records = _repository.GetPagedUsers(page, pageSize);
+            return Clients.Caller.populateGrid(records);
+        }
+    }
+}
---
+++ WebGridExample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace WebGridExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
